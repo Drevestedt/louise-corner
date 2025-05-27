fe221203f9bcac52d082bc2221d480bdc44a12b3
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-27 20:50:47 +0200

    Initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+
+  return (
+    <div className="min-h-screen bg-yellow-100 flex flex-col items-center justify-center text-center">
+      <h1>Test</h1>
+      <p>We're testing the page</p>
+    </div>
+  );
+};
+
+export default Home;
