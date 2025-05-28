c62dff71450844110333541cdee58ad4be740dd9
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-28 22:04:50 +0200

    Install Tailwind

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Home() {
 
   return (
-    <div>
-      <h1>Test</h1>
-      <p>We're testing the page</p>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-yellow-100">
+      <h1 className="text-4xl font-bold">Test</h1>
+      <p className="text-lg ">We're testing the page</p>
     </div>
   );
 };
