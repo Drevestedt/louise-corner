8683593cdb9acc6515f74812aa7bcf79f2fcfe6c
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-29 20:00:10 +0200

    Add mobile navbar menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="flex items-center justify-between bg-amber-900 px-8 py-4">
       <h1 className="text-3xl text-stone-100 hover:text-red-300"><Link to="/">Louise´s Corner</Link></h1>
-      <ul className="flex gap-4 text-lg text-stone-100">
+
+      {/* Hamburger icon for mobile */}
+      <button className="md:hidden text-stone-100 hover:text-red-300"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
+      </button>
+
+      {/* Desktop menu */}
+      <ul className="hidden md:flex gap-4 text-lg text-stone-100">
         <li className="hover:text-red-300"><Link to="/">Home</Link></li>
         <li className="hover:text-red-300"><Link to="/menu">Menu</Link></li>
         <li className="hover:text-red-300"><Link to="/bookTable">Book Table</Link></li>
         <li className="hover:text-red-300"><Link to="/about">About</Link></li>
         <li className="hover:text-red-300"><Link to="/contact">Contact</Link></li>
       </ul>
+
+      {/* Mobile menu */}
+      <div className={`fixed top-12 right-0 h-half w-1/3 mt-3 max-w-xs bg-amber-900 text-stone-100 text-center transform transition-transform duration-300 ease-in-out z-50
+        ${isMenuOpen ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <ul className="flex flex-col gap-2 text-lg text-stone-100">
+          <li className="hover:text-red-300"><Link to="/" onClick={() => setIsMenuOpen(false)} >Home</Link></li>
+          <li className="hover:text-red-300"><Link to="/menu" onClick={() => setIsMenuOpen(false)} >Menu</Link></li>
+          <li className="hover:text-red-300"><Link to="/bookTable" onClick={() => setIsMenuOpen(false)} >Book Table</Link></li>
+          <li className="hover:text-red-300"><Link to="/about" onClick={() => setIsMenuOpen(false)} >About</Link></li>
+          <li className="hover:text-red-300 mb-3"><Link to="/contact" onClick={() => setIsMenuOpen(false)} >Contact</Link></li>
+        </ul>
+      </div>
     </nav>
   )
 }
