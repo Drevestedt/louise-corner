7264aa3162c6e96b1ddaf569b388933e20c4e8e9
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-05-31 21:13:13 +0200

    Add menu page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ frontend/src/pages/Menu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Menu() {
+
+  return (
+    <div>
+      <h1 className="flex flex-col justify-center items-center font-[Playfair] text-4xl">Test</h1>
+    </div> {/* #TODO: Keep debugging h1 not showing up on page */}
+  );
+}
