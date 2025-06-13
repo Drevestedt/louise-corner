69cb349c3d7833d075aee549e510f5d2bb02f8a3
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-06-13 18:45:18 +0200

    Fix running port for dev and prod in server

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,9 @@ connectDB().then(database => {
     }
   });
 
-  app.listen(3001, () => {
-    console.log("Server is running on: http://localhost:3001");
+  const PORT = process.env.PORT || 3001;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
   });
 });
