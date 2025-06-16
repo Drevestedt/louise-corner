335c96f35272e4513fc5a3c8886c286ca34fa2d7
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-06-16 11:18:15 +0200

    Update routing for frontend in production

--- frontend/src/components/Booking.jsx
+++ frontend/src/components/Booking.jsx
@@ -24,7 +24,7 @@ export default function Booking() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
       });
-      
+
       const data = await response.json();
 
       if (response.ok) {
